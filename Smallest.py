48b4f94018cabe1d0cdabcf471e0a85c434c01f7
Ishhan Goyal <goyalishaan@gmail.com>
2025-01-28 14:51:34 +0530

    Add files via upload

---
+++ Smallest.py
@@ -0,0 +1,7 @@
+a = float(input("Enter the 1st number: "))
+b = float(input("Enter the 2nd number: "))
+c = float(input("Enter the 3rd number: "))
+
+smallest = min(a, b, c)
+
+print(f"The smallest number among {a}, {b}, and {c} is: {smallest}")
