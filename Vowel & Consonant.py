e42b577245c63d86192400f2d551706b54357efb
Ishhan Goyal <goyalishaan@gmail.com>
2025-02-25 10:22:03 +0530

    Delete Vowel & Consonant.py

--- Vowel & Consonant.py
+++
@@ -1,5 +0,0 @@
-char = input("Enter a character: ").lower()
-if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
-    print(char, "is a Vowel.")
-else:
-    print(char, "is a Consonant.")
