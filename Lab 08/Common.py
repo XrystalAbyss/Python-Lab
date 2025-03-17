2e7b12e661afad06568c07738b446dfd7ea964b2
Ishhan Goyal <goyalishaan@gmail.com>
2025-03-17 22:58:34 +0530

    Add files via upload

---
+++ Lab 08/Common.py
@@ -0,0 +1,7 @@
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+common_elements = set1 & set2
+if common_elements:
+    print("Common elements:", common_elements)
+else:
+    print("No common elements")
